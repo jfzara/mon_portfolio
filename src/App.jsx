7e716e9325b8865fbec89bc379701d008204fa23
jfzara <zarajeanfabrice@gmail.com>
2025-10-30 03:58:04 -0400

    police modifee

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
-import './App.css'
-import Layout from "./components/Layout.jsx"
-function App() {
- 
+import { createGlobalStyle } from "styled-components";
+import Layout from "./components/Layout";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Inter', sans-serif;
+    margin: 0;
+    padding: 0;
+    background: #1E1E1E;
+    color: #E7E5E4;
+  }
+`;
+
+function App() {
   return (
     <>
-      <Layout/>
+      <GlobalStyle />
+      <Layout />
     </>
   )
 }
 
-export default App
+export default App;
